4cbc3b0666ad3d4edb7c8f4097fd2ce398374fb5
Fateh Abdinassir <ffabdi1239@gmail.com>
2026-04-09 00:17:11 +0300

    Added vercel domain in index.js

--- Server/index.js
+++ Server/index.js
@@ -12,6 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'https://dynamic-qr-attendance.vercel.app',
     'https://dynamic-qr-attendance-abdinassirfatehs-projects.vercel.app'
   ],
   credentials: true
@@ -30,6 +31,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       'http://localhost:5173',
+      'https://dynamic-qr-attendance.vercel.app',
       'https://dynamic-qr-attendance-abdinassirfatehs-projects.vercel.app'
     ],
     methods: ['GET', 'POST'],
